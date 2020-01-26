906694748673e6cdb66d8044def68f74a9155d28
EVAN VOORDECKERS <u1087559@lab1-27.eng.utah.edu>
2020-01-26 16:40:30 -0700

    Added testing file, and config to allow to debug

---
+++ Personal_Testing/testing.c
@@ -0,0 +1,8 @@
+int main(int argc, char** argv)
+{
+    short arr[] = { 2, 4, 6, 8, 10, 12, 14, 16 };
+    short* p1 = arr + 10;
+    short* p2 = arr + 6;
+    typedef char * word;
+    return 0;
+}
